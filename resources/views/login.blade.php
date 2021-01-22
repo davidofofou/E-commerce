104ef88a29e6ff2f5eed387b00642b54fb566988
davidofofou <guifofou@gmail.com>
2021-01-22 15:54:55 +0100

    initialisation du controller users

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -4,16 +4,18 @@
     <div class="container custom-login">
         <div class="row">
             <div class="col-sm-4 offset-sm-4">
-                <form>
+                <form action="login" method="POST">
+                    @csrf
                         <div class="form-group">
                             <label for="exampleFormControlInput1" class="form-label">Email address</label>
-                            <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="name@example.com">
+                            <input type="email" name="email" class="form-control" id="exampleFormControlInput1" placeholder="name@example.com">
                         </div>
                         <div class="form-group">
                             <label for="exampleFormControlInput1" class="form-label">Password </label>
-                            <input type="password" class="form-control" id="exampleFormControlInput1" placeholder="password">
+                            <input type="password" name="password" class="form-control" id="exampleFormControlInput1" placeholder="password">
                         </div>
-                        <button type="submit" class="btn btn-primary">Submit</button>
+                        <br>
+                        <button type="submit" class="btn btn-primary">Login</button>
                 </form>  
 
             </div>
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\UsersController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -16,3 +16,4 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('login');
 });
+Route::post('/login',[UsersController::class,'login']); 
